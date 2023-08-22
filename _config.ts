75c42b24789e4424286b1a0c7cb994911e2f2948
Ahmad Farhan <45879091+afokay@users.noreply.github.com>
2023-08-23 05:40:01 +0700

    Update _config.ts

--- _config.ts
+++ _config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "lume/plugins/tailwindcss.ts";
 import postcss from "lume/plugins/postcss.ts";
 
 const site = lume({
+    location: new URL("https://files-2oa.pages.dev"),
     components: {
         variable: "components",
     },
