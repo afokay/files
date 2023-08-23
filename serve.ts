51ca838363bb0db359302769509c1fff599a1656
afokay <ahmadnitro@proton.me>
2023-08-23 20:03:18 +0700

    update 404

--- serve.ts
+++ serve.ts
@@ -1,9 +1,17 @@
 import Server from "lume/core/server.ts";
+import expires from "lume/middlewares/expires.ts";
+import notFound from "lume/middlewares/not_found.ts";
 
 const server = new Server({
   port: 8000,
   root: `${Deno.cwd()}/_site`,
 });
+
+server.use(expires());
+server.use(notFound({
+  root: `${Deno.cwd()}/_site`,
+  page404: "/404/",
+}));
 
 server.start();
 
